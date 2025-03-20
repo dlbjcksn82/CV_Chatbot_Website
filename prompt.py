7c2fcc41dfb43bbc06b2482f8434cfdc008dd319
dlbjcksn82 <dj918@york.ac.uk>
2025-03-20 11:41:35 +0000

    Updated to use GPT40-mini model hosted by open AI. Requires a Openai api token to use and be saved as a system variable.

--- prompt.py
+++ prompt.py
@@ -2,15 +2,7 @@ from langchain.prompts import PromptTemplate
 
 prompt_template = PromptTemplate(
     input_variables=["chat_history", "cv_info", "user_input"],
-    template="""You are an AI assistant helping users understand a person's CV.
-    Use the provided CV information to answer questions accurately. If the CV 
-    does not contain the answer, say 'I do not know.' and do not make up an 
-    answer. Do not repeat the question. Provide detailed, structured 
-    responses, explaining key points with useful context. Elaborate when 
-    possible by adding background information to make the answer more useful 
-    to a potential employer. Avoid one-word answers unless absolutely 
-    necessary, but try to keep answers concise and to the point.
-
+    template="""
     Here is the conversation history so far:
     {chat_history}
 
