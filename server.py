0b86281606b68dbda82bd469982a22114e8e3ef5
dlbjcksn82 <dj918@york.ac.uk>
2025-03-20 11:33:16 +0000

    Updated to use GPT40-mini model hosted by open AI. Requires a Openai api token to use and be saved as a system variable.

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
+import os
 from flask import Flask, request, jsonify, render_template
-from chatbot import chat_with_gemma
+from chatbot import chat_with_gpt
 from flask_cors import CORS
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 # enable CORS for /chat endpoint
 CORS(app, resources={r"/chat": {"origins": "*"}})
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route('/chat', methods=['POST', 'OPTIONS'])
 def chat():
-    if request.method == 'OPTIONS': #handle preflight request
+    if request.method == 'OPTIONS':  # handle preflight request
         return jsonify({"message": "CORS preflight successful"}), 200
 
     data = request.get_json()
@@ -20,12 +25,13 @@ def chat():
         return jsonify({"error": "No message provided"}), 400
 
     user_input = data["message"]
-    response = chat_with_gemma(user_input)
+    response = chat_with_gpt(user_input)
 
     if not response:
         return jsonify({"error": "No response generated"}), 500
 
     return jsonify({"response": response}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
